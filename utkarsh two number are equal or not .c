50f6ee24d8a786fb68465e87f4b55a4c84b3f6cb
Utkarsh yadav <utkarshking0101@gmail.com>
2026-02-16 10:57:58 +0530

    5

---
+++ utkarsh two number are equal or not .c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter two number =");
+    scanf("%d%d",&a,&b);
+    if(a==b){
+        printf("numer are equal");
+    }
+    else{
+        printf("number are not equal");
+    }
+
+    return 0;
+}
