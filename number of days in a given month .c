7e57b4e76718973233c94d2c30a0c8f79a20e0cb
Utkarsh yadav <utkarshking0101@gmail.com>
2026-02-23 12:11:59 +0530

    Add files via upload

---
+++ number of days in a given month .c
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include<math.h>
+int main() {
+    int m;
+    printf("enter the m=");
+    scanf("%d",&m);
+    switch(m)
+    {
+        case 1:
+        printf("number of days=31");
+        break;
+        case 2:
+        printf("number of days=28");
+        break;
+        case 3:
+        printf("number of days=31");
+        break;
+        case 4:
+        printf("number of days=30");
+        break;
+        case 5:
+        printf("number of days=31");
+        break;
+        case 6:
+        printf("number of days=30");
+        break;
+        case 7:
+        printf("number of days=31");
+        break;
+        case 8:
+        printf("number of days=31");
+        break;
+       case 9:
+        printf("number of days=30");
+        break;
+        case 10:
+        printf("number of days=31");
+        break;
+        case 11:
+        printf("number of days=30");
+        break;
+        case 12:
+        printf("number of days=31");
+        break;
+     
+    }
+    return 0;
+}
